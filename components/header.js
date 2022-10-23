0e5da750d04cd8123e6220ba5e1dae4201e97788
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-10-23 13:35:08 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,11 +1,20 @@
+//import react 
 import React  from "react";
 
+// class for the header component// extends word used to export app.js
 export class Header extends React.Component{
+    
     render(){
+        
+        // returning components 
+        
         return(
             <div>
                 <h1>My header component</h1>
             </div>
-        );
-    }
-}
+            
+        ); //return 
+        
+    } //render 
+    
+} //class
