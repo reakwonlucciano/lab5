95aed76c7e16f2d9ccdb48d7bb683ce1a68851e9
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-10-23 13:28:56 +0100

    Update content.js

--- components/content.js
+++ components/content.js
@@ -1,12 +1,17 @@
+//import react 
 import React from "react";
 
+// class for the middle content component// extends word used to export app.js
 export class Content extends React.Component {
     render() {
+        
+        //returning components
+        
         return (
             <div>
                 <h1>Hello World!!!!!!!!!!</h1>
                 <h2>It is {new Date().toLocaleTimeString()}.</h2>
             </div>
-        );
-    }
-}
+        ); //return 
+    } //render
+} //class
